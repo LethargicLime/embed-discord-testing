11c589136160b2d6fc359ab6c758c4b4b043002f
LethargicLime <majesticallime@gmail.com>
2025-06-05 16:43:29 -0700

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const url = ref( null );
+
+function handleFileUpload(e) {
+  const f = e.target.files[0];
+
+  if (f && f.type.startsWith("video/")) {
+    url.value = URL.createObjectURL(f);
+  } else {
+    alert("video nimwit");
+  }
+}
+</script>
+
+<template>
+  <div class="absolute">
+    <input type="file" accept="video/*" @change="handleFileUpload" class="mt-2 ml-2 border-1 w-48 rounded border-solid"/>
+
+    <video v-if="url" controls :src="url" class="w-screen h-screen">
+      
+    </video>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
