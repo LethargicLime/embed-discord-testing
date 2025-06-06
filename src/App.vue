6bded79010862ae3ed93ae47980f59c2d71cc826
LethargicLime <majesticallime@gmail.com>
2025-06-06 00:08:41 -0700

    dynamic meta 	modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,28 @@ const loadVideo = async () => {
     const t = await getDownloadURL(result.items[0]);
 
     url.value = t;
+
+    setMetadata("og:video", url.value);
   }
+}
+
+function setMetadata(property:any, content:any) {
+  let meta = document.querySelector(`meta[property='${property}']`);
+  
+  if (!meta) {
+    meta = document.createElement("meta");
+
+    meta.setAttribute("property", property);
+    document.head.appendChild(meta);
+  }
+
+  meta.setAttribute("content", content);
 }
 
 onMounted(() => {
   loadVideo();
-})
+});
+
 </script>
 
 <template>
